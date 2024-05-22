df300204e2c641baa639c0e8e3ddae3c42d26570
Arvee <arveeculanag@gmail.com>
2024-05-22 09:31:22 +0800

    improve hero section

--- src/script.js
+++ src/script.js
@@ -1,17 +1,12 @@
-const heroGreetingSpan = document.querySelector('.hero-greeting--highlight')
-const heroGreetingItems = [
-  "Website",
-  "Python",
-  "Android",
-  "Backend"
-]
+const heroGreetingSpan = document.querySelector(".hero-greeting--highlight");
+const heroGreetingItems = ["Website", "Python", "Android"];
 
-var timeLeft = heroGreetingItems.length
-var startTimer = setInterval(function() {
+let timeLeft = heroGreetingItems.length;
+let startTimer = setInterval(() => {
   if (timeLeft <= 0) {
     timeLeft = heroGreetingItems.length;
   }
 
   heroGreetingSpan.innerHTML = heroGreetingItems[timeLeft - 1];
   timeLeft -= 1;
-}, 1000)
+}, 1000);
